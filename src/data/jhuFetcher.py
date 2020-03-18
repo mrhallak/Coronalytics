ece544f8c2579fe1c3c7dcadf4072c1be24d33e8
Mohamad Hallak <hallaksec@gmail.com>
2020-03-18 19:28:45 +0200

    Finalize JhuFetcher.py

--- src/data/jhuFetcher.py
+++ src/data/jhuFetcher.py
@@ -1,27 +1,28 @@
 import requests
 import logging
+
 from datetime import datetime
 
 class JhuFetcher:
     @staticmethod
-    def fetch(chunk_size=8192):
+    def fetch(current_execution_date: str, chunk_size=8192, **context: dict):
         try:
-            current_date = datetime.today().strftime('%m-%d-%Y')
-            logging.info(f"Fetching data from Johns Hopkins University - Daily Reports ({current_date})")
+            execution_date_reformat = datetime.strptime(current_execution_date, '%Y-%m-%d').strftime('%m-%d-%Y')
+            logging.info(f"Fetching data from Johns Hopkins University - Daily Reports ({current_execution_date})")
 
-            url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{current_date}.csv'
+            url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{execution_date_reformat}.csv'
             
-            local_filename = f"{current_date}.csv"
+            write_path = f"/tmp/{current_execution_date}.csv"
 
             with requests.get(url, stream=True) as r:
                 r.raise_for_status()
 
-                with open(local_filename, 'wb') as f:
+                with open(write_path, 'wb') as f:
                     for chunk in r.iter_content(chunk_size=chunk_size): 
                         if chunk:
                             f.write(chunk)
-            
-            return local_filename
+
+            return current_execution_date
         
         except Exception as e:
             logging.error(e)
