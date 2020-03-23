2544c212791cb31618add226f452ca417c4e0986
Mohamad Hallak <hallaksec@gmail.com>
2020-03-23 15:45:07 +0200

    Remove gcp related items

--- docs/SERVICES.md
+++ docs/SERVICES.md
@@ -8,13 +8,8 @@ Postgres is a SQL database that we will use to store our transformed data.
 ### Grafana
 Grafana is a visualization tool that we will use to visualize the data loaded into Postgres.
 
-### Jupyter
-
 ### Apache Airflow
 Airflow is an orchestration tool that allows us to run a DAG (Direct Acyclic Graph), in other words a set of tasks in a particular order. We will be using Airflow to schedule our DAG, which is the ETL pipeline, daily after Johns Hopkins University updates their data.
-
-#### Authenticating with Google Cloud
-Please follow this [tutorial][1] in order to authenticate with Google Cloud.
 
 ### Portainer
 Portainer allows you to build and manage your docker containers.
@@ -23,7 +18,6 @@ Portainer allows you to build and manage your docker containers.
 | Service | Port |
 | --- | --- |
 | Airflow | 8080 |
-| Jupyter | 8888 |
 | Portainer | 9090 |
 | Postgres | 5432 |
 | Grafana | 3000 |
--- src/pipeline.py
+++ src/pipeline.py
@@ -7,16 +7,13 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
-# Google Cloud DataProc
-from airflow.contrib.operators.dataproc_operator import DataprocClusterCreateOperator, DataprocClusterDeleteOperator, DataProcPySparkOperator
-
 # Other
 from data.jhuFetcher import JhuFetcher
 from utils.storage import Storage
 
 default_args = {
     'owner': 'airflow',
-    'start_date': airflow.utils.dates.days_ago(58),
+    'start_date': airflow.utils.dates.days_ago(60),
     'depends_on_past': False,
     'email': [''],
     'email_on_failure': False,
@@ -31,8 +28,6 @@ dag = DAG(
     description='DAG to extract, transform and load data related to the Corona virus (COVID-19)',
     schedule_interval=datetime.timedelta(days=1),
 )
-
-# gcp_storage = Storage(project_id=os.environ['GCP_PROJECT_ID'])
 
 fetch_data = PythonOperator(
     task_id='fetch_data',
@@ -55,44 +50,5 @@ delete_local_file = BashOperator(
     dag=dag,
 )
 
-# create_dataproc_cluster = DataprocClusterCreateOperator(
-#     task_id='create_dataproc_cluster',
-#     dag=dag,
-#     cluster_name=os.environ['GCP_CLUSTER_NAME'],
-#     region=os.environ['GCP_CLUSTER_REGION'],
-#     project_id=os.environ['GCP_PROJECT_ID'],
-#     master_machine_type='n1-standard-1',
-#     worker_machine_type='n1-standard-1',
-#     num_workers=2,
-#     storage_bucket=os.environ['GCP_STORAGE_BUCKET']
-# )
-
 fetch_data >> [load_data_in_pg]
 load_data_in_pg >> [delete_local_file]
-
-
-# submit_job = DataProcPySparkOperator(
-#     task_id='count_items',
-#     dag=dag,
-#     main="file://main.py",
-#     job_name="count_items",
-#     cluster_name=os.environ['GCP_CLUSTER_NAME'],
-#     region=os.environ['GCP_CLUSTER_REGION']
-# )
-
-# delete_dataproc_cluster = DataprocClusterDeleteOperator(
-#     task_id='delete_dataproc_cluster',
-#     dag=dag,
-#     cluster_name=os.environ['GCP_CLUSTER_NAME'],
-#     region=os.environ['GCP_CLUSTER_REGION'],
-#     project_id=os.environ['GCP_PROJECT_ID']
-# )
-
-# fetch_data.set_upstream(upload_data_to_gcs)
-# create_dataproc_cluster.set_upstream(submit_job)
-
-# submit_job.set_upstream(
-#     [
-#         delete_dataproc_cluster
-#     ]
-# )
