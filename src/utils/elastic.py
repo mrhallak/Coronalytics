26bc76984a31ff2070d14f206f5f0d7afdf42ca6
Mohamad Hallak <hallaksec@gmail.com>
2020-03-29 20:36:48 +0300

    Code documentation and formatting.

--- src/utils/elastic.py
+++ src/utils/elastic.py
@@ -1,13 +1,39 @@
 import logging
 import datetime
+import os
+
 from elasticsearch import Elasticsearch, helpers
+from typing import Iterator
+
 
 class Elastic:
     def __init__(self, **kwargs):
+        """Constructor, creates an
+        elasticsearch client and checks if
+        we are able to connect to the cluster.
+        
+        Raises:
+            ValueError: Unable to connect to the ElasticSearch cluster
+        """        
         try:
-            hosts = [
-                f"http://elastic:changeme@elasticsearch:9200"
-            ]
+            username = (
+                kwargs["username"]
+                if "username" in kwargs
+                else os.environ["ELASTICSEARCH_USERNAME"]
+            )
+            password = (
+                kwargs["password"]
+                if "password" in kwargs
+                else os.environ["ELASTICSEARCH_PASSWORD"]
+            )
+            host = (
+                kwargs["host"] if "host" in kwargs else os.environ["ELASTICSEARCH_HOST"]
+            )
+            port = (
+                kwargs["port"] if "port" in kwargs else os.environ["ELASTICSEARCH_PORT"]
+            )
+
+            hosts = [f"http://{username}:{password}@{host}:{port}"]
 
             self.elastic_client = Elasticsearch(hosts)
 
@@ -16,6 +42,7 @@ class Elastic:
 
         except Exception as e:
             logging.error(e)
+            raise
 
     def __enter__(self) -> object:
         """This function allows us to use
@@ -39,12 +66,39 @@ class Elastic:
         self.elastic_client.transport.close()
         logging.info("Closed connection to ElasticSearch")
 
+    def create_index(self, index_name: str, mapping: dict) -> None:
+        """This function creates an index on
+        the ElasticSearch cluster and ignores any
+        errors if the index already exists.
+        
+        Arguments:
+            index_name {str} -- Name of the index to be created
+            mapping {dict} -- Dict mapping keys to the type of their values
+        """        
+        self.elastic_client.indices.create(index=index_name, body=mapping, ignore=[400])
 
-    def index(self, index_name, data, chunk_size=100000):
+    def index(self, index_name: str, data: Iterator, chunk_size: int = 100000):
+        """This function streams to the cluster in bulk the data
+        contained in an iterator.
+        
+        Arguments:
+            index_name {str} -- Index to store the documents in
+            data {Iterator} -- Documents iterator
+        
+        Keyword Arguments:
+            chunk_size {int} -- Size of the chunks to stream (default: {100000})
+        """           
         failed = 0
         total = 0
 
-        for success, info in helpers.streaming_bulk(self.elastic_client, data, chunk_size=chunk_size, index=index_name, max_retries=20, request_timeout=120):
+        for success, _ in helpers.streaming_bulk(
+            self.elastic_client,
+            data,
+            chunk_size=chunk_size,
+            index=index_name,
+            max_retries=20,
+            request_timeout=120,
+        ):
             if not success:
                 failed += 1
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,96 +1,23 @@
-from io import StringIO, TextIOBase
-from csv import DictReader, QUOTE_ALL
-from dateutil import parser
-from datetime import datetime
-from typing import Optional, Any, Iterator
-
-
-# https://stackoverflow.com/a/12604375/2000875
-class StringIteratorIO(TextIOBase):
-    def __init__(self, iter: Iterator[str]):
-        self._iter = iter
-        self._buff = ""
-
-    def readable(self) -> bool:
-        return True
-
-    def _read1(self, n: Optional[int] = None) -> str:
-        while not self._buff:
-            try:
-                self._buff = next(self._iter)
-            except StopIteration:
-                break
-
-        ret = self._buff[:n]
-        self._buff = self._buff[len(ret) :]
-        return ret
-
-    def read(self, n: Optional[int] = None) -> str:
-        line = []
-
-        if n is None or n < 0:
-            while True:
-                m = self._read1()
-
-                if not m:
-                    break
-
-                line.append(m)
-        else:
-            while n > 0:
-                m = self._read1(n)
-
-                if not m:
-                    break
-
-                n -= len(m)
-                line.append(m)
-
-        return "".join(line)
-
-
-def file_to_iterable(
-    file_path: str, fields: tuple, current_execution_date: str, delimiter: str = ","
-) -> StringIO:
-
-    current_execution_date = datetime.strptime(
-        current_execution_date, "%Y-%m-%d"
-    ).date()
+def generate_document(body: dict, current_execution_date: str) -> dict:
+    """This function helps by convering
+    the data scraped from JHU to a
+    document that we will store in 
+    ElasticSearch.
     
-    data = DictReader(
-        open(file_path), fieldnames=fields, delimiter=delimiter, quoting=QUOTE_ALL
-    )
+    Arguments:
+        body {dict} -- JSON object received
+        current_execution_date {str} -- DAG's execution date
+    
+    Returns:
+        dict -- Transformed object
+    """    
+    doc = {
+        "updated": current_execution_date,
+        "confirmed": body["Confirmed"],
+        "deaths": body["Deaths"],
+        "recovered": body["Recovered"],
+        "location": {"lon": body["Long_"], "lat": body["Lat"]},
+        "country_name": body["Country_Region"],
+    }
 
-    # Skip header row
-    next(data)
-
-    file_iterator = StringIteratorIO(
-        (
-            delimiter.join(
-                map(
-                    clean_csv_value,
-                    (
-                        row["province"],
-                        row["country"],
-                        row["last_update"],
-                        row["confirmed"],
-                        row["deaths"],
-                        row["recovered"],
-                        row["latitude"],
-                        row["longitude"],
-                    ),
-                )
-            )
-            + "\n"
-            for row in data
-        )
-    )
-
-    return file_iterator
-
-
-def clean_csv_value(value: Optional[Any]) -> str:
-    if value is None or value == "":
-        return r"\N"
-
-    return str(value).replace("\n", "\\n").replace(",", "")
+    return doc
