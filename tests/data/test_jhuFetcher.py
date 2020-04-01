6412246533f92aaf3bb42b0177298cf63efac6a0
Mohamad Hallak <hallaksec@gmail.com>
2020-04-01 19:39:58 +0300

    Add some tests to the jhuFetcher

---
+++ tests/data/test_jhuFetcher.py
@@ -0,0 +1,20 @@
+import pytest
+
+from datetime import datetime
+from src.data.jhuFetcher import JhuFetcher
+
+CURRENT_EXECUTION_DATE = datetime.now()
+
+
+@pytest.fixture()
+def request():
+    return JhuFetcher.fetch_by_country(current_execution_date=CURRENT_EXECUTION_DATE)
+
+
+class TestJhuFetcher:
+    def test_fetch_by_country_response_type(self, data):
+        assert type(data) == list
+        assert type(data[0]) == dict
+
+    def test_fetch_by_country_valid_response(self, data):
+        assert len(data) > 0
