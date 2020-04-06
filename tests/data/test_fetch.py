49fc39adb72e00d2955baa0b29435a1228cb3541
Mohamad Hallak <hallaksec@gmail.com>
2020-04-06 20:32:22 +0300

    Update tests

---
+++ tests/data/test_fetch.py
@@ -0,0 +1,33 @@
+import pytest
+import requests
+
+from datetime import datetime
+from src.data.fetch import JhuFetcher
+
+
+
+@pytest.fixture()
+def response():
+    return JhuFetcher.fetch_by_country()
+
+
+class TestJhuFetcher:
+    def test_fetch_by_country_fails(self, response):
+        assert response.status_code != 200
+
+    def test_fetch_by_country_succeeds(self, response):
+        assert response.status_code == 200
+
+    def test_fetch_by_country_response_type(self, response):
+        data = response.json()['features']
+
+        assert type(data) == list
+        assert type(data[0]) == dict
+
+    def test_fetch_by_country_valid_response(self, response):
+        data = response.json()
+
+        assert "features" in data
+
+        assert len(data) > 0
+        assert response.json()
---
+++ tests/data/test_transform.py
@@ -0,0 +1,52 @@
+import pytest
+
+from datetime import datetime
+from src.data.transform import Transformer
+
+
+CURRENT_EXECUTION_DATE = datetime.now()
+
+
+DOCUMENT_TEMPLATE = {
+    "OBJECTID": 16, 
+    "Country_Region": "Switzerland",
+    "Last_Update": 1585506823000, 
+    "Lat": 46.8182,
+    "Long_": 8.2275, 
+    "Confirmed": 14829,
+    "Deaths": 300, 
+    "Recovered": 1595, 
+    "Active": 12934
+}
+
+RESPONSE_TEMPLATE = {
+    "updated": CURRENT_EXECUTION_DATE,
+    "confirmed": 14829,
+    "deaths": 300,
+    "recovered": 1595,
+    "location": {
+        "lon": 8.2275,
+        "lat": 46.8182
+    },
+    "country_name": "Switzerland",
+}
+
+
+class TestTransformer:
+    @pytest.fixture()
+    def transformer(self):
+        return Transformer()
+
+    def test_generate_document_output(self, transformer):
+        generated_doc = transformer.generate_document(DOCUMENT_TEMPLATE, CURRENT_EXECUTION_DATE)
+
+        assert generated_doc == RESPONSE_TEMPLATE
+
+    def test_generate_document_output_type(self, transformer):
+        generated_doc = transformer.generate_document(DOCUMENT_TEMPLATE, CURRENT_EXECUTION_DATE)
+
+        assert type(generated_doc) == dict
+
+    def test_generate_document_errors(self, transformer):
+        with pytest.raises(TypeError):
+            transformer.generate_document("", CURRENT_EXECUTION_DATE)
